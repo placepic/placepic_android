30280a9bb91742d704c67bcefe63657a90318bae
jinsu4755 <parkjinsu4755@daum.net>
2020-07-01 02:08:29 +0900

    #5 [Feat] 로그인 : Layout 작성 1. 추후 변경 가능성이 있음.

---
+++ app/src/main/java/place/pic/ui/login_group/LoginActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.login_group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
