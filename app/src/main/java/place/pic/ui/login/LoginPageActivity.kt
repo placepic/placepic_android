ea7b1f8ec393de4f05efa5a2e4653d6a373ba693
jinsu4755 <parkjinsu4755@gmail.com>
2020-07-03 03:47:46 +0900

    [Fix] merge후 다른 브랜치에서 에러 없도록 코드 수정

--- app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
+++ app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
@@ -1,12 +1,8 @@
 package place.pic.ui.login
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_login_page.*
 import place.pic.R
-import place.pic.ui.login.group.GroupActivity
 
 class LoginPageActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
