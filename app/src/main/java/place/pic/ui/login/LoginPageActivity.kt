bd3dd146a710ef8fce5600db42d841be39de20fc
jinsu4755 <parkjinsu4755@gmail.com>
2020-07-03 03:45:45 +0900

    [Fix] merge후 다른 브랜치에서 에러 없도록 코드 수정

--- app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
+++ app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
@@ -8,26 +8,9 @@ import kotlinx.android.synthetic.main.activity_login_page.*
 import place.pic.R
 import place.pic.ui.login.group.GroupActivity
 
-class LoginPageActivity : AppCompatActivity(), View.OnClickListener {
+class LoginPageActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
-        init()
-    }
-
-    private fun init() {
-        setButtonEventListener()
-    }
-
-    private fun setButtonEventListener() {
-        btn_home_signin.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.btn_home_signin -> {
-               
-            }
-        }
     }
 }
