d36d9fb698a16215b8b2474a4edaa9b57dfc6f85
jinsu4755 <parkjinsu4755@daum.net>
2020-07-04 06:37:31 +0900

    [Fix] 로그인 화면 : 화면 디자인 단위 수정
    디자인과 협의후 변경사항 적용
    1. 15dp -> 18dp 정도로 수정

---
+++ app/src/main/java/place/pic/ui/extands/customTextChangedListener.kt
@@ -0,0 +1,21 @@
+package place.pic.ui.extands
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+fun EditText.customTextChangedListener(
+    textChanged : (CharSequence?) -> Unit
+)
+{
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) = Unit
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int)=Unit
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){
+            textChanged(s)
+        }
+
+    })
+}
