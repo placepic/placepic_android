a171e562f5bae28c7da72195d0a0b4ea7d3fe3f9
jinsu4755 <parkjinsu4755@daum.net>
2020-07-05 02:39:17 +0900

    [Feat] 로그인 화면 : 로그인 관련 Activity 작성
    1. 초기 페이지에서 로그인 버튼을 눌러 로그인 Layout 불러옴
    2. 로그인 화면에서 이메일 형식 확인
    3. EditText 에 정보가 들어오면 로그인 버튼 활성화ㅏ

--- app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
+++ app/src/main/java/place/pic/ui/login/LoginPageActivity.kt
@@ -1,12 +1,33 @@
 package place.pic.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login_page.*
 import place.pic.R
 
-class LoginPageActivity : AppCompatActivity(){
+class LoginPageActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        init()
+    }
+
+    private fun init(){
+        buttonMapping()
+    }
+
+    private fun buttonMapping(){
+        btn_goto_login.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btn_goto_login -> {
+                val gotoLoginIntent = Intent(this,LoginActivity::class.java)
+                startActivity(gotoLoginIntent)
+            }
+        }
     }
 }
