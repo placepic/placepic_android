1685b888e1335e6c80d3ea6a10063544bbe233f8
jinsu4755 <parkjinsu4755@daum.net>
2020-07-06 23:13:28 +0900

    [Feat] 그룹 리스트 : 테스트용 데이터 작성
    일부 코드또한 수정

---
+++ app/src/main/java/place/pic/ui/group/ExistGroupListViewHolder.kt
@@ -0,0 +1,11 @@
+package place.pic.ui.group
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ExistGroupListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(customData: ListGroupData) {
+
+    }
+}
---
+++ app/src/main/java/place/pic/ui/group/ListGroupData.kt
@@ -0,0 +1,8 @@
+package place.pic.ui.group
+
+data class ListGroupData(
+    val url:String,
+    val title:String,
+    val people_count:Int,
+    val write_count:Int
+)
