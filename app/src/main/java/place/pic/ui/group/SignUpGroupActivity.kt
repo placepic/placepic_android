5e1bbafb09ea358c7d90ccc533e5864b640a2206
jinsu4755 <parkjinsu4755@daum.net>
2020-07-06 15:07:03 +0900

    [Feat] 그룹 가입 신청 페이지 : 레이아웃 작성

---
+++ app/src/main/java/place/pic/ui/group/SignUpGroupActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class SignUpGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView( R.layout.activity_sign_up_group)
+    }
+}
