86703ae79ccea1363ac855d873d2e34cf74146b6
jinsu4755 <parkjinsu4755@daum.net>
2020-07-06 14:04:55 +0900

    [Feat] 승인 대기중 그룹 화면 : 승인 대기중 그룹 목록 레이아웃 작성

---
+++ app/src/main/java/place/pic/ui/group/WaitGroupActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class WaitGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wait_group)
+    }
+}
