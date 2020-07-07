10dae28ef4c384a16fd1c8d971a688ca48805db5
Malibin <nightmare73@naver.com>
2020-07-08 05:50:13 +0900

    [Feat] BaseResponse 객체 작성

--- app/src/main/java/place/pic/data/remote/PlacePicService.kt
+++ app/src/main/java/place/pic/data/remote/PlacePicService.kt
@@ -24,7 +24,7 @@ interface PlacePicService {
 
         fun getInstance() = instance ?: synchronized(this) {
             instance ?: provideService(PlacePicService::class.java)
-                .also { instance = it }
+                .apply { instance = this }
         }
     }
 }
---
+++ app/src/main/java/place/pic/data/remote/response/BaseResponse.kt
@@ -0,0 +1,13 @@
+package place.pic.data.remote.response
+
+/**
+ * Created By Malibin
+ * on 7월 08, 2020
+ */
+
+data class BaseResponse<T>(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: T
+)
