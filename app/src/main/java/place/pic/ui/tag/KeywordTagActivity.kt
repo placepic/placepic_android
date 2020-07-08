a81f76db5d82540005906aaf8eaffa427db04bc3
kimdahyee <diana6777@naver.com>
2020-07-08 15:13:52 +0900

    [Feat] 유용한 태그 선택 Layout 작성
    1. 태그 하나 이상 선택 시 태그 등록하기 버튼 색 변경

--- app/src/main/java/place/pic/ui/tag/KeywordTagActivity.kt
+++ app/src/main/java/place/pic/ui/tag/KeywordTagActivity.kt
@@ -4,6 +4,7 @@ package place.pic.ui.tag
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.activity_keyword_tag.*
@@ -16,7 +17,7 @@ import place.pic.R
 
 class KeywordTagActivity : AppCompatActivity() {
 
-    private val chipList = mutableListOf<Chip>()
+    private val keywordTagChipList = mutableListOf<Chip>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +29,15 @@ class KeywordTagActivity : AppCompatActivity() {
         val chipGroup = chipgroup_keyword_tag
         for (tags in restaurantKeywordTagList) {
             val chip = KeywordChipFactory.newInstance(layoutInflater) //object method 호출하기
-            chipList.add(chip)
+            keywordTagChipList.add(chip)
             chip.isClickable = true
             chip.text = tags
             chipGroup.addView(chip)
         }
 
-        for (i in 0 until chipList.size) { //chipList.forEach
-            chipList[i].setOnClickListener{view ->
-                if (checkChip()) {//true
+        for (i in 0 until keywordTagChipList.size) { //chipList.forEach로도 가능
+            keywordTagChipList[i].setOnClickListener{view ->
+                if (checkChipIsChecked()) {//true
                     tv_tag_save.setTextColor(Color.parseColor("#FFFFFF"))
                     keyword_tag_save.setBackgroundResource(R.drawable.rectangle_main_color)
                     keyword_tag_save.isClickable = true
@@ -49,23 +50,17 @@ class KeywordTagActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkChip(): Boolean {
+    private fun checkChipIsChecked(): Boolean {
         var checkResult = false
-        for (i in 0 until chipList.size) {
-            if (chipList[i].isChecked) //true
+        for (i in 0 until keywordTagChipList.size) {
+            if (keywordTagChipList[i].isChecked) //true
                 checkResult = true
         }
         return checkResult
     }
 
-    private fun init() {
-        tv_tag_save.setTextColor(Color.parseColor("#F1F4F5"))
-        keyword_tag_save.setBackgroundResource(R.drawable.rectangle_gray_f1f4f5)
-        keyword_tag_save.isClickable = false
-    }
-
     fun onClick(view: View) {
-
+        Toast.makeText(this@KeywordTagActivity, "클릭 가능", Toast.LENGTH_SHORT).show()
     }
 }
 
--- app/src/main/java/place/pic/ui/tag/UsefulTagActivity.kt
+++ app/src/main/java/place/pic/ui/tag/UsefulTagActivity.kt
@@ -1,8 +1,11 @@
 package place.pic.ui.tag
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.activity_useful_tag.*
 import place.pic.R
 
@@ -12,21 +15,50 @@ import place.pic.R
  */
 
 class UsefulTagActivity : AppCompatActivity() {
+
+    private val usefulTagChipList = mutableListOf<Chip>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_useful_tag)
 
-        val resturantUsefulTagList = arrayOf("24시간", "콘센트있음", "화장실공용")
+        val resturantUsefulTagList
+                = arrayOf("24시간", "콘센트있음", "화장실공용", "태그4", "태그5")
 
         val chipGroup = chipgroup_useful_tag
         for (tags in resturantUsefulTagList) {
             val chip = KeywordChipFactory.newInstance(layoutInflater) //object method 호출하기
+            usefulTagChipList.add(chip)
             chip.isClickable = true
             chip.text = tags
             chipGroup.addView(chip)
         }
+
+        for (i in 0 until usefulTagChipList.size) { //chipList.forEach로도 가능
+            usefulTagChipList[i].setOnClickListener{view ->
+                if (checkChipIsChecked()) {//true
+                    tv_useful_tag_save.setTextColor(Color.parseColor("#FFFFFF"))
+                    useful_tag_save.setBackgroundResource(R.drawable.rectangle_main_color)
+                    useful_tag_save.isClickable = true
+                } else {
+                    tv_useful_tag_save.setTextColor(Color.parseColor("#4F4F4F"))
+                    useful_tag_save.setBackgroundResource(R.drawable.rectangle_gray_f1f4f5)
+                    useful_tag_save.isClickable = false
+                }
+            }
+        }
+    }
+
+    private fun checkChipIsChecked(): Boolean {
+        var checkResult = false
+        for (i in 0 until usefulTagChipList.size) {
+            if (usefulTagChipList[i].isChecked) //true
+                checkResult = true
+        }
+        return checkResult
     }
 
     fun onClick(view: View) {
+        Toast.makeText(this@UsefulTagActivity, "클릭 가능", Toast.LENGTH_SHORT).show()
     }
 }
