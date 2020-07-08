90635a3e518e76b1b558f1c947118bc8d3c94f75
jinsu4755 <parkjinsu4755@daum.net>
2020-07-08 21:45:47 +0900

    [Feat] Retrofit 서버 연결관련 로그 찍기.

--- app/src/main/java/place/pic/data/remote/RetrofitCreator.kt
+++ app/src/main/java/place/pic/data/remote/RetrofitCreator.kt
@@ -1,5 +1,7 @@
 package place.pic.data.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,5 +13,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 fun <T> provideService(clazz: Class<T>): T = Retrofit.Builder()
     .baseUrl(PlacePicService.BASE_URL)
     .addConverterFactory(GsonConverterFactory.create())
+    .client(httpLoggingClient())
     .build()
     .create(clazz)
+
+private fun httpLoggingClient(): OkHttpClient {
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+    return OkHttpClient.Builder().addInterceptor(interceptor).build()
+}
