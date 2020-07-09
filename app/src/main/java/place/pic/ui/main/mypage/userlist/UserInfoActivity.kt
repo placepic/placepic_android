8d8b166c0f379e9acb2c1f27ef0c68aa21429686
jinsu4755 <parkjinsu4755@daum.net>
2020-07-09 20:13:48 +0900

    [Feat] 관리자뷰 : 관리자뷰 완성.

---
+++ app/src/main/java/place/pic/ui/main/mypage/userlist/UserInfoActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.main.mypage.userlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class UserInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
---
+++ app/src/main/java/place/pic/ui/main/mypage/userlist/UserListData.kt
@@ -0,0 +1,6 @@
+package place.pic.ui.main.mypage.userlist
+
+data class UserListData(
+    val userName:String,
+    val userGroup:String
+)
