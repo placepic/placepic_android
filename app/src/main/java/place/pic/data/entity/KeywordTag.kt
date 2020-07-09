38e59d2e03b1241c59115b21f207e9598cfd86db
kimdahyee <diana6777@naver.com>
2020-07-09 20:28:54 +0900

    [Feat] 기본 태그(Keyword) Activity 관련
    1. 이전 view로부터 categoryIdx 받아오기 구현
    2. 선택딘 chip 정보 ArrayList<KeywordTag>로 전달하기 구현

---
+++ app/src/main/java/place/pic/data/entity/KeywordTag.kt
@@ -0,0 +1,14 @@
+package place.pic.data.entity
+
+import java.io.Serializable
+
+/**
+ * Created By kimdahyee
+ * on 07월 09일, 2020
+ */
+
+data class KeywordTag (
+    var tagIdx: Int,
+    var tagName: String
+) : Serializable
+ 
