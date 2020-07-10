4318f43f856ff4146803854cd1f0ef414f5faa27
jinsu4755 <parkjinsu4755@daum.net>
2020-07-10 21:41:48 +0900

    [Feat] 싱글턴 방식 SharedPreference

--- app/src/main/java/place/pic/data/PlacepicAuthRepository.kt
+++ app/src/main/java/place/pic/data/PlacepicAuthRepository.kt
@@ -1,48 +1,48 @@
 package place.pic.data
 
+import android.app.Activity
 import android.content.Context
-import android.content.SharedPreferences
 
-class PlacepicAuthRepository private constructor() {
+
+class PlacepicAuthRepository private constructor(context: Context) {
     companion object {
+        private const val PLACEPIC_AUTH = "placepic_auth"
+
         @Volatile
         private var instance: PlacepicAuthRepository? = null
 
-        private lateinit var sharedPreference: SharedPreferences
-
-        private var PLACEPIC_AUTH = "placepic_auth"
-
         @JvmStatic
-        fun getInstance(context: Context) = instance ?: synchronized(this) {
-            instance ?: PlacepicAuthRepository().also {
-                instance = it
-                sharedPreference = context.getSharedPreferences(PLACEPIC_AUTH,Context.MODE_PRIVATE)
+        fun getInstance(context:Context) = instance ?: synchronized(this) {
+            instance ?: PlacepicAuthRepository(context).apply {
+                instance = this
             }
         }
     }
-    
-    private var editer = sharedPreference.edit()
+
+    private val sharedPreferences = context.getSharedPreferences(context.packageName,Activity.MODE_PRIVATE)
+
+    private var editor = sharedPreferences.edit()
 
     val userToken : String?
-        get() = sharedPreference.getString(PLACEPIC_AUTH,"")
+        get() = sharedPreferences.getString(PLACEPIC_AUTH,"")
 
-    val groupId : Int?
-        get() = sharedPreference.getInt(PLACEPIC_AUTH,0)
+   val groupId : Int?
+        get() = sharedPreferences.getInt(PLACEPIC_AUTH,0)
 
     fun saveUserToken(userToken:String){
-        editer.putString(PLACEPIC_AUTH,userToken).apply()
+        editor.putString(PLACEPIC_AUTH,userToken).apply()
     }
 
     fun saveGroupId(groupId: Int) {
-        editer.putInt(PLACEPIC_AUTH,groupId).apply()
+        editor.putInt(PLACEPIC_AUTH,groupId).apply()
     }
 
     fun removeUserToken(){
-        editer.remove(PLACEPIC_AUTH)
+        editor.remove(PLACEPIC_AUTH)
     }
 
     fun removeGroupId(){
-        editer.remove(PLACEPIC_AUTH)
+        editor.remove(PLACEPIC_AUTH)
     }
     
 }
