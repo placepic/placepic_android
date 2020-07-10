526e176636f7cc5ea23c817ae24b5c30a58f1def
jinsu4755 <parkjinsu4755@daum.net>
2020-07-10 17:43:17 +0900

    [Feat] 싱글톤 sharedPreference 제작

---
+++ app/src/main/java/place/pic/data/PlacepicAuthRepository.kt
@@ -0,0 +1,47 @@
+package place.pic.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PlacepicAuthRepository private constructor() {
+    companion object {
+        @Volatile
+        private var instance: PlacepicAuthRepository? = null
+        private lateinit var sharedPreference: SharedPreferences
+
+        private var PLACEPIC_AUTH = "placepic_auth"
+
+        @JvmStatic
+        fun getInstance(context: Context) = instance ?: synchronized(this) {
+            instance ?: PlacepicAuthRepository().also {
+                instance = it
+                sharedPreference = context.getSharedPreferences(PLACEPIC_AUTH,Context.MODE_PRIVATE)
+            }
+        }
+    }
+
+    private var editer = sharedPreference.edit()
+
+    val userToken : String?
+        get() = sharedPreference.getString(PLACEPIC_AUTH,"")
+
+    val groupId : Int?
+        get() = sharedPreference.getInt(PLACEPIC_AUTH,0)
+
+    fun saveUserToken(userToken:String){
+        editer.putString(PLACEPIC_AUTH,userToken).apply()
+    }
+
+    fun saveGroupId(groupId: Int) {
+        editer.putInt(PLACEPIC_AUTH,groupId).apply()
+    }
+
+    fun removeUserToken(){
+        editer.remove(PLACEPIC_AUTH)
+    }
+
+    fun removeGroupId(){
+        editer.remove(PLACEPIC_AUTH)
+    }
+    
+}
