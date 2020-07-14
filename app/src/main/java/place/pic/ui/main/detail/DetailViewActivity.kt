275ac5109b98401e0c19955a3ccfbbb532aeaaae
jinsu4755 <parkjinsu4755@daum.net>
2020-07-15 06:11:57 +0900

    [Feat] 디테일 뷰 : 서버 연결 및 로직
    1. 디테일 관련 서버를 불러오는 로직 연결.
    2. 사진을 보여줄 뷰페이저 설정

--- app/src/main/java/place/pic/ui/main/detail/DetailViewActivity.kt
+++ app/src/main/java/place/pic/ui/main/detail/DetailViewActivity.kt
@@ -3,10 +3,52 @@ package place.pic.ui.main.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import place.pic.R
+import place.pic.data.PlacepicAuthRepository
+import place.pic.data.remote.PlacePicService
+import place.pic.data.remote.response.DetailResponse
+import place.pic.ui.extands.customEnqueue
 
 class DetailViewActivity : AppCompatActivity() {
+
+    private var imageList:List<String>? = null
+
+    lateinit var detailviewPagerAdapter: DetailViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_view)
+
+        init()
+    }
+
+    private fun init(){
+
+    }
+
+    /* 서버 연결 */
+    private fun requestToDetailView(){
+        val token = PlacepicAuthRepository.getInstance(this).userToken?: return
+
+        PlacePicService
+            .getInstance()
+            .requestDetail(
+                token = token,
+                placeIdx = 60
+            ).customEnqueue(
+                onSuccess = {response ->
+                    val serverData = response.body()?.data?:return@customEnqueue
+                    bindingDetail(serverData)
+                }
+            )
+    }
+
+    private fun bindingDetail(detailResponse: DetailResponse) {
+        viewPager = (ViewPager) findViewById(R.id.view);
+        adapter = new Adapter(this);
+        viewPager.setAdapter(adapter);
+    }
+
+    private fun setViewPager(){
+
     }
 }
