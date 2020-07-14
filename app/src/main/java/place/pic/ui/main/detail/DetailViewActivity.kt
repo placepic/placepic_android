aa87a28550a5a7da8693b11a248c200c21d71dab
jinsu4755 <parkjinsu4755@daum.net>
2020-07-15 03:11:19 +0900

    [Feat] Detail View : Layout 작성 완료

---
+++ app/src/main/java/place/pic/ui/main/detail/DetailViewActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.main.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class DetailViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_view)
+    }
+}
