464278f1e5e5e7f69f2a09d5d90542022abd2cf1
leesujung-dodung <leesujung0917@gmail.com>
2020-07-18 06:30:56 +0900

    [Feat] 스플래쉬

--- app/src/main/java/place/pic/ui/main/SplashActivity.kt
+++ app/src/main/java/place/pic/ui/main/SplashActivity.kt
@@ -2,10 +2,15 @@ package place.pic.ui.main
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.lottie.LottieAnimationView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_splash.*
 import place.pic.R
 import place.pic.ui.login.LoginPageActivity
@@ -16,18 +21,17 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-
-        lottie.addAnimatorListener(object : AnimatorListenerAdapter(){
+        lottie.addAnimatorListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator?) {
                 val intent = Intent(this@SplashActivity, LoginPageActivity::class.java)
                 startActivity(intent)
                 finish()
-                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             }
         })
+
         lottie.playAnimation()
-
+        window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
-
 
 }
