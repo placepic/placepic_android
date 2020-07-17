9a968d49d2028249a6e628fcc7e292f3eebc530b
jinsu4755 <parkjinsu4755@daum.net>
2020-07-17 22:51:01 +0900

    [Fix] 디테일 뷰에서 정보 넘기기.
    디테일뷰에서 북마크가 되어있는경우 뒤로가기시, 삭제시 placeIdx를 전달.

--- app/src/main/java/place/pic/ui/main/detail/DetailViewActivity.kt
+++ app/src/main/java/place/pic/ui/main/detail/DetailViewActivity.kt
@@ -1,10 +1,10 @@
 package place.pic.ui.main.detail
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail_view.*
 import place.pic.R
@@ -12,9 +12,7 @@ import place.pic.data.PlacepicAuthRepository
 import place.pic.data.entity.Place
 import place.pic.data.remote.PlacePicService
 import place.pic.data.remote.request.RequestToPlacceIdx
-import place.pic.data.remote.response.DetailLikerResponse
 import place.pic.data.remote.response.DetailResponse
-import place.pic.data.remote.response.Like
 import place.pic.data.remote.response.Uploader
 import place.pic.ui.dialog.SimpleDialog
 import place.pic.ui.extands.customEnqueue
@@ -100,6 +98,14 @@ class DetailViewActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onBackPressed() {
+        if (getMyBookmarkButtonStatus()) {
+            val intent = Intent().apply { putExtra("BookmarkedEventPlaceIdx",placeIdx) }
+            setResult(Activity.RESULT_OK, intent)
+        }
+        super.onBackPressed()
+    }
+
 
     /* 서버 연결 */
     //리스트 불러오기
@@ -174,7 +180,9 @@ class DetailViewActivity : AppCompatActivity(), View.OnClickListener {
                 placeIdx = placeIdx
             ).customEnqueue(
                 onSuccess = {
-                    onBackPressed()
+                    val intent = Intent().apply { putExtra("DelEventPlaceIdx",placeIdx) }
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
                 }
             )
     }
