1bb5c95d90b3242b60b569ebdb69180d4777a7e9
jinsu4755 <parkjinsu4755@gmail.com>
2020-09-20 19:34:19 +0900

    [Feat] 로그인,회원가입,그룹 인증 페이지: 에니메이션과 로직 추가 수정
    1. Sqash로 일부 잘못된 코드 수정
    2. 프로필 작성 부분 애니메이션 느낌 수정.

--- app/src/main/java/place/pic/ui/group/InputUserInfoAnimActivity.kt
+++ app/src/main/java/place/pic/ui/group/InputUserInfoAnimActivity.kt
@@ -1,5 +1,6 @@
 package place.pic.ui.group
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_input_user_info_anim.*
 import place.pic.R
+import place.pic.ui.main.MainActivity
 import place.pic.ui.util.animation.BindLayoutAnimation
 import place.pic.ui.util.animation.nextActivityAnimation
 import place.pic.ui.util.animation.previousActivityAnimation
@@ -56,7 +58,6 @@ class InputUserInfoAnimActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun buttonEventMapping() {
         btn_input_user_info_real_start.setOnClickListener(this)
-        btn_input_user_info_end_input_name.setOnClickListener(this)
         btn_input_user_info_end_input_part.setOnClickListener(this)
         img_input_user_info_top_back_btn.setOnClickListener(this)
     }
@@ -101,8 +102,11 @@ class InputUserInfoAnimActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(button: View?) {
         when (button!!.id) {
             R.id.btn_input_user_info_end_input_part ->{showInputNameFormEvent()}
-            R.id.btn_input_user_info_end_input_name -> {}
-            R.id.btn_input_user_info_real_start ->{}
+            R.id.btn_input_user_info_real_start ->{
+                val gotoMainIntent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(gotoMainIntent)
+                finishAffinity()
+            }
             R.id.img_input_user_info_top_back_btn -> onBackPressed()
         }
 
