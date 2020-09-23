cca1425e6081518f78f3c863eb96dafd4190c2bc
Malibin <nightmare73@naver.com>
2020-09-24 01:23:24 +0900

    [Feat] 공모전 디테일 레이아웃 작성

---
+++ app/src/main/java/place/pic/ui/main/home/banner/detail/BannerDetailActivity.kt
@@ -0,0 +1,12 @@
+package place.pic.ui.main.home.banner.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import place.pic.R
+
+class BannerDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_banner_detail)
+    }
+}
