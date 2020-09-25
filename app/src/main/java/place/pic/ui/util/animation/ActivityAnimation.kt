80d579f14d2d710f3f79114719eeb4932ddabcec
jinsu4755 <parkjinsu4755@gmail.com>
2020-09-26 02:45:40 +0900

    [Fix] 로그인,회원가입,그룹 가입: 애니메이션 피드백 수정

--- app/src/main/java/place/pic/ui/util/animation/ActivityAnimation.kt
+++ app/src/main/java/place/pic/ui/util/animation/ActivityAnimation.kt
@@ -1,12 +1,11 @@
 package place.pic.ui.util.animation
 
 import android.app.Activity
-import android.content.Context
 import place.pic.R
 
 fun Activity.nextActivityAnimation(){
     this.overridePendingTransition(
-        R.anim.load_fade_in,
+        R.anim.Activity_load_fade_in,
         R.anim.alpah_slide_out_right_to_left
     )
 }
@@ -14,6 +13,6 @@ fun Activity.nextActivityAnimation(){
 fun Activity.previousActivityAnimation(){
     this.overridePendingTransition(
         R.anim.alpah_slide_in_left_to_right,
-        R.anim.load_fade_out
+        R.anim.Activity_load_fade_out
     )
 }
